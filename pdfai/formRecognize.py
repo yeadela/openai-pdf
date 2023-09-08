c87d74bcb6732a7a0dd1fee6f151345686285936
qliu <906101221@qq.com>
2023-09-08 17:53:04 +0800

    update

--- pdfai/formRecognize.py
+++ pdfai/formRecognize.py
@@ -56,7 +56,7 @@ def test():
     os.environ["STORAGE_KEY"] = "/7kV9kaYPQAyiY45m+MWtI4xmicB8Mn7iMBeSquNbHHo4cxN98ESVtVMLzp/ReqGjZBgRy+yhLwp+AStUkqZUg=="
     os.environ["STORAGE_CONTAINER"] = "star5container"
 
-    os.environ["RECOGNIZER_ENDPOINT"] ="https://star5recoginzer.cognitiveservices.azure.com"
+    os.environ["RECOGNIZER_ENDPOINT"] ="https://star5recognizer.cognitiveservices.azure.com"
     os.environ["RECOGNIZER_KEY"] ="4e554529206f4ee5acb5a26fa153607a"
 
     os.environ["COSMOS_URL"] ="https://star5.documents.azure.com:443"
@@ -69,7 +69,9 @@ def test():
 
     #store file to blob storage
     storage_account_url = f'https://{os.environ.get("STORAGE_ACCOUNT")}.blob.core.windows.net'
+    
     storage_container = os.environ.get("STORAGE_CONTAINER")
+    print( f'{storage_account_url}/{storage_container}/' + filename)
     # blob_service_client = BlobServiceClient(account_url=storage_account_url,credential=os.environ.get("STORAGE_KEY"))
     # blob_client = blob_service_client.get_blob_client(container=storage_container, blob=file.filename)
     # blob_client.upload_blob(file)
@@ -81,7 +83,7 @@ def test():
                     credential=AzureKeyCredential(os.environ.get("RECOGNIZER_KEY")),
                 )
     poller = doc_analysis_client.begin_analyze_document_from_url(
-                    "star5model", document = f'{storage_account_url}/{storage_container}/' + filename
+                    "star5model", document_url = f'{storage_account_url}/{storage_container}/' + filename
                 )
 
     result = poller.result()
