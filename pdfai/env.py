8d07e9f1d94857f0c83a973274f986ec6b50aa97
yeadela <yeyinia@163.com>
2023-09-12 13:54:45 +0800

    update

--- pdfai/env.py
+++ pdfai/env.py
@@ -2,7 +2,7 @@ import os
 import pinecone
 
 def init():
-    os.environ["OPENAI_API_KEY"] ="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"
+    os.environ["OPENAI_API_KEY"] ="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"
     pinecone.init(
     api_key="ec70078b-04f8-4d91-8ebc-98ad36e923c1",
     environment="asia-southeast1-gcp-free"
